427fa9b85fce67b02223bc0c9dc90bc6136cb87f
maslyankov <maslyankov@users.noreply.github.com>
2020-07-21 15:14:22 +0300

    Create split_images.py
    Script splits All the images in specified folder in two.

---
+++ split_images.py
@@ -0,0 +1,36 @@
+# Script splits all the images in specified folder in two.
+from PIL import Image
+from pip._vendor.distlib.compat import raw_input
+import os
+
+path = raw_input('Input Path (enter current dir): ')
+if path == '':
+    path="./"
+
+dirs = os.listdir( path )
+
+def is_jpg(filename):
+    try:
+        i=Image.open(filename)
+        return i.format =='JPEG'
+    except IOError:
+        print("Skipping " + filename + " - not JPEG")
+        return False
+
+def split_img():
+    print("Starting...")
+    for item in dirs:
+        if os.path.isfile(path+item) & is_jpg(path+item):
+            img = Image.open(path+item)
+            width, height = img.size
+            f, e = os.path.splitext(path + item)
+
+            cropped = img.crop((0, 0, width - 0, height - 7760/2))
+            cropped.save(f + '_cam1.jpg', 'JPEG', quality=100)
+            print("Outputting: " + f + '_cam1.jpg')
+
+            cropped = img.crop((0, 7760/2, width - 0, height - 0))
+            cropped.save(f + '_cam2.jpg', 'JPEG', quality=100)
+            print("Outputting: " + f + '_cam2.jpg')
+
+split_img()
